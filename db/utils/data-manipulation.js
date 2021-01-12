fb3aca31c27e18ce6fff7da1d448e0ba097bcb57
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-01-12 13:55:42 +0000

    utils seeding functions complete

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -5,15 +5,36 @@
 
 // 946684800000 milliseconds between 1970 and 2000
 
-exports.formatArticlesData = (rawData) => {
+exports.formatArticlesData = rawData => {
+  const formattedData = rawData.map(data => {
+    const newData = { ...data };
+    newData.created_at = new Date(data.created_at);
+    return newData;
+  });
+  return formattedData;
+};
 
-    const formattedData = rawData.map(data => {
-        const newData = { ...data };
-        const newDate = new Date(data.created_at)
-        console.log(newDate)
-        newData.created_at = newDate
-        return newData
-    })
-    return formattedData;
-}
+exports.formatCommentsData = (commentsArr, articleLookup) => {
+  return commentsArr.map(comment => {
+    const newComment = { ...comment };
 
+    newComment.article_id = articleLookup[newComment.belongs_to];
+    delete newComment.belongs_to;
+
+    newComment.username = newComment.created_by;
+    delete newComment.created_by;
+
+    newComment.created_at = new Date(newComment.created_at);
+
+    return newComment;
+  });
+};
+
+exports.createArticleLookup = articlesArr => {
+  const lookup = {};
+  articlesArr.forEach(article => {
+    lookup[article.title] = article.article_id;
+  });
+
+  return lookup;
+};
---
+++ snippets.js
@@ -0,0 +1,50 @@
+/*
+
+fe
+array.forEach(function(item) {
+  // body
+});
+
+cl / ce
+console.log() / console.error();
+
+js / jp
+
+JSON.stringify(obj); / JSON.parse(obj);
+
+req
+const packageName = require('packageName');
+
+mde
+module.exports = {
+  
+};
+
+fof
+for (const item of object) {
+  
+}
+
+fin
+for (const item in object) {
+  
+}
+
+anfn
+(params) => {
+  
+}
+
+prom
+return new Promise((resolve, reject) => {
+  
+});
+
+thenc
+.then((result) => {
+  
+}).catch((err) => {
+  
+});
+
+*/
