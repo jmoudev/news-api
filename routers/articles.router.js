fdbfb29e8adbaaaf56a9a9fcd9f46e8d4ad2cce5
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-01-14 09:27:17 +0000

    post comment complete

--- routers/articles.router.js
+++ routers/articles.router.js
@@ -3,9 +3,11 @@ const {
   getArticle,
   patchArticle
 } = require('../controllers/articles.controllers');
+const { postComment } = require('../controllers/comments.controllers');
 
 const articlesRouter = express.Router();
 
 articlesRouter.route('/:article_id').get(getArticle).patch(patchArticle);
+articlesRouter.route('/:article_id/comments').post(postComment);
 
 module.exports = articlesRouter;
