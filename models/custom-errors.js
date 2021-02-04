b363a0752341a7983f9316f2ca1965240886c394
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-04 15:05:00 +0000

    custom errors added

---
+++ models/custom-errors.js
@@ -0,0 +1,5 @@
+const custom400Err = { status: 400, msg: 'Bad Request' };
+const custom404Err = { status: 404, msg: 'Not Found' };
+const custom405Err = {};
+
+module.exports = { custom400Err, custom404Err, custom405Err };
