a7c81f4aaf066d678eee27ea64e1c0e8766c4db2
JJYoko101 <mould_joe@hotmail.com>
2021-02-08 14:19:47 +0000

    enabled all cors requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const apiRouter = require('./routers/api.router');
 const {
   handleRouteNotFound,
@@ -8,7 +9,9 @@ const {
   handleServerErrors
 } = require('./controllers/errors.controllers');
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/api', apiRouter);
 app.all('/*', handleRouteNotFound);
 
